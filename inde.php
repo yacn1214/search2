b6bb60d1d63db35e778ae55e2850581324ed04b1
yacn1214 <yacn1214@gmail.com>
2022-01-16 19:49:31 +0330

    add curd

--- inde.php
+++ inde.php
@@ -19,8 +19,8 @@ $db = new db;
         }
     </style>
 </head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<body class="bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Search free...</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -56,9 +56,20 @@ if (isset($_POST['search'])) {
     foreach($db->search($se) as $ok){
 
 ?>
-            <div style="display: inline-flex ">
+            <div style="display: inline-flex ;padding: 9px ">
 <div class="card" style="width: 18rem;">
-    <img src="https://is3-ssl.mzstatic.com/image/thumb/Purple116/v4/48/94/3d/48943d38-4843-7014-8eed-ea6963b02ee7/AppIcon-0-1x_U007emarketing-0-7-0-85-220.png/1200x630wa.png" class="card-img-top" alt="...">
+    <img src=<?php
+    if($ok['cat']=="pdf"){
+        echo "https://is3-ssl.mzstatic.com/image/thumb/Purple116/v4/48/94/3d/48943d38-4843-7014-8eed-ea6963b02ee7/AppIcon-0-1x_U007emarketing-0-7-0-85-220.png/1200x630wa.png";
+
+    }if($ok['cat']=="video"){
+    echo "https://www.keycdn.com/img/blog/video-optimization.png";
+    }if($ok['cat']=="rar"){
+        echo "https://www.itechguides.com/wp-content/uploads/2020/03/How-to-Open-RAR-Files-on-Windows-10-scaled.jpg";
+    }if($ok['cat']=="zip"){
+        echo "https://www.freeiconspng.com/thumbs/file-zip-icon/file-zip-icon-2.png";
+    }
+    ?> class="card-img-top" style="width:287px;height:150px" alt="...">
     <div class="card-body">
         <h5 class="card-title"><?php echo $ok['title'] ?></h5>
         <p class="card-text"><?php echo $ok['caption'] ?></p>
@@ -90,5 +101,130 @@ if (isset($_POST['search'])) {
     }
 }
 ?>
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+    <!-- Section: Social media -->
+    <section
+            class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+    >
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i>yacn1414
+                    </h6>
+                    <p style="text-align:right;direction: rtl">
+                        سلام من یه برنامه نویس ساده ام توی سهند تبریز :)
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Products
+                    </h6>
+                    <p>
+                        <a href="?php" class="text-reset">php</a>
+                    </p>
+                    <p>
+                        <a href="?python" class="text-reset">python</a>
+                    </p>
+                    <p>
+                        <a href="?c" class="text-reset">c++</a>
+                    </p>
+                    <p>
+                        <a href="?a" class="text-reset">all programing language</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Useful links
+                    </h6>
+                    <p>
+                        <a href="../about.php" class="text-reset">about</a>
+                    </p>
+                    <p>
+                        <a href="../" class="text-reset">main page</a>
+                    </p>
+                    <p>
+                        <a href="../contact.php" class="text-reset">contact</a>
+                    </p>
+                    <p>
+                        <a href="../help.php" class="text-reset">Help</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Contact
+                    </h6>
+                    <p><i class="fas fa-home me-3"></i> Tabriz Sahand</p>
+                    <p>
+                        <i class="fas fa-envelope me-3"></i>
+                        yacn1214@gmail.com
+                    </p>
+                    <p><i class="fas fa-phone me-3"></i> + 98 930 174 14 97</p>
+                    <p><i class="fas fa-print me-3"></i> + 98 930 174 14 97</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2021 Copyright:
+        <a class="text-reset fw-bold" href="https://yacn.ir/">yacn.ir</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
 </body>
 </html>
